e249e5cd0e6cbe64a08f79e216cc7429abd1aa3d
yusef-atef <97854649+yusef-atef@users.noreply.github.com>
2024-06-25 16:28:37 +0300

    Update Click counter.py

--- Click counter.py
+++ Click counter.py
@@ -6,7 +6,7 @@ def on_click(event):
     cc = counter.get()
     cc += 1
     counter.set(cc)
-    if cc > 999999999:
+    if cc > 999999:
         on_reset(event)
 
 
@@ -15,7 +15,7 @@ def on_reset(event):
 
 app = tk.Tk()
 app.title("Click Counter")
-app.geometry("350x205")
+app.geometry("200x210")
 app.resizable(width=False, height=False)
 
 counter = IntVar(value=0)
@@ -28,14 +28,14 @@ control_frame.pack()
 canvas = tk.Canvas(master=control_frame, width=350, height=120)
 canvas.pack()
 
-click = canvas.create_oval(100, 5, 205, 110, fill="green")
-reset = canvas.create_oval(215, 55, 275, 115, fill="red")
+click = canvas.create_oval(30, 5, 125, 110, fill="green")
+reset = canvas.create_oval(130, 60, 180, 110, fill="red")
 
 canvas.tag_bind(click, "<1>", on_click)
 canvas.tag_bind(reset, "<1>", on_reset)
 
-text1 = canvas.create_text(152, 58, text="Click", fill='#FF5733', font=("Helvetica", 20, "bold"))
-text2 = canvas.create_text(245, 85, text="Reset", fill="black", font=("Helvetica", 10, "bold"))
+text1 = canvas.create_text(77, 58, text="Click", fill='#FF5733', font=("Helvetica", 20, "bold"))
+text2 = canvas.create_text(155, 85, text="Reset", fill="black", font=("Helvetica", 10, "bold"))
 
 canvas.tag_bind(text1, "<1>", on_click)
 canvas.tag_bind(text2, "<1>", on_reset)
